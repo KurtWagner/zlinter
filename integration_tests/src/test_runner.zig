fc3e0ac09ddd99ed1c7eb8c57ebb52471d490416
Kurt Wagner <1269283+KurtWagner@users.noreply.github.com>
2025-06-29 17:26:51 +1000

    Add rule no_orelse_unreachable

--- integration_tests/src/test_runner.zig
+++ integration_tests/src/test_runner.zig
@@ -1,3 +1,7 @@
+const ansi_red_bold = "\x1B[31;1m";
+const ansi_green_bold = "\x1B[32;1m";
+const ansi_reset = "\x1B[0m";
+
 pub fn main() !void {
     var mem: [32 * 1024]u8 = undefined;
     var fba = std.heap.FixedBufferAllocator.init(&mem);
@@ -9,10 +13,10 @@ pub fn main() !void {
 
     for (builtin.test_functions) |t| {
         t.func() catch |err| {
-            try std.fmt.format(out, "Integration test for '{s}'' failed: {}\n", .{ args[1], err });
+            try std.fmt.format(out, "Integration test '{s}' {s}failed{s}: {}\n", .{ args[1], ansi_red_bold, ansi_reset, err });
             continue;
         };
-        try std.fmt.format(out, "Integration test for '{s}'' passed\n", .{args[1]});
+        try std.fmt.format(out, "Integration test '{s}' {s}passed{s}\n", .{ args[1], ansi_green_bold, ansi_reset });
     }
 }
 
