6fe04d2b39aa31164a7a5efc225e95ec292317be
Kurt <1269283+KurtWagner@users.noreply.github.com>
2025-07-02 21:53:43 +1000

    Add more deprecated test cases and simplify classification (#14)
    Previously it was using the source index based approach, as we're
    working off of nodes in the AST, it doesn't really make a lot of sense
    to lean too heavily on source index.

--- integration_tests/test_cases/no_deprecation/no_deprecation.input.zig
+++ integration_tests/test_cases/no_deprecation/no_deprecation.input.zig
@@ -1,33 +1,50 @@
 /// Deprecated: don't use this
 const message = "hello";
 
+/// Deprecated: Just because
+pub const MyDeprecatedEnum = enum { a, b };
+
 /// Deprecated - function not good
 fn doNothing() void {}
 
 pub const MyStruct = struct {
     /// Deprecated: also not this
-    field_a: u32 = 10,
-    field_b: u32 = 11,
+    deprecated_field: u32 = 10,
+    other_field: u32 = 11,
+
+    state: test_case_references.State = .really_deprecated,
+
+    deprecated_enum_field: MyDeprecatedEnum = .a,
+
+    const deprecated_container_decl: MyDeprecatedEnum = .b;
 
     /// Deprecated - not good
     pub fn alsoDoNothing(self: MyStruct) u32 {
-        return self.field_a;
+        return self.deprecated_field;
     }
 };
 
 pub fn main() void {
-    std.log.err("Message: {s}", .{message});
+    const data = test_case_references.MyDeprecatedData{
+        .deprecated_field = 10,
+        .ok_field = 12,
+    };
+
+    std.log.err("Message: {s} {d} {d}", .{ message, data.deprecated_field, data.ok_field });
 
     const indirection = message;
     std.log.err("Message: {s}", .{indirection});
 
     doNothing();
 
-    const me = MyStruct{};
-    std.log.err("Fields: {d} {d}", .{ me.field_a, me.field_b });
+    const me = MyStruct{ .state = .really_deprecated };
+    std.log.err("Fields: {d} {d}", .{ me.deprecated_field, me.field_b });
 
     // TODO: Fails on 0.15 but works on 0.14 - need to work out why. Commented out for now
     _ = me.alsoDoNothing();
+
+    std.log.err("Hello {s}", .{test_case_references.doNotCallDeprecated()});
 }
 
 const std = @import("std");
+const test_case_references = @import("../../src/test_case_references.zig");
