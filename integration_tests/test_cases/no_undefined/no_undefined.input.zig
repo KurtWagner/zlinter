3a5919adb8f268629fe50c901c5da4d636811ffd
Kurt Wagner <1269283+KurtWagner@users.noreply.github.com>
2025-07-07 13:09:39 +1000

    Skip no undefined when in test or deinit fn
    This uses the new / wip lineage data found in the doc on load

--- integration_tests/test_cases/no_undefined/no_undefined.input.zig
+++ integration_tests/test_cases/no_undefined/no_undefined.input.zig
@@ -5,4 +5,25 @@ fn main() void {
     std.fmt.log("{s}", .{ undefined, message });
 }
 
+const MyStruct = struct {
+    name: []const u8,
+
+    pub fn deinit(self: *MyStruct) void {
+        self.* = undefined;
+    }
+};
+
 const std = @import("std");
+
+// We expect any undefined with a test to simply be ignored as really we expect
+// the test to fail if there's issues
+test {
+    var this_is_a_test_so_who_cares: u32 = undefined;
+
+    const Struct = struct {
+        var nested_who_cares: f32 = undefined;
+    };
+
+    this_is_a_test_so_who_cares = 0;
+    _ = Struct{};
+}
