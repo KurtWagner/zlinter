2d1f40f2ddee886984b4deb654679000e2169918
Kurt Wagner <1269283+KurtWagner@users.noreply.github.com>
2025-07-13 06:26:15 +1000

    WIP

--- src/rules/field_ordering.zig
+++ src/rules/field_ordering.zig
@@ -44,6 +44,16 @@ fn run(
             node.toNodeIndex(),
         ) == null) continue :skip;
 
+        var insertion_order = std.ArrayList(std.zig.Ast.Node.Index).init(allocator);
+        defer insertion_order.deinit();
+
+        var sorted_order = std.PriorityQueue(
+            Field,
+            void,
+            Field.cmp,
+        ).init(allocator, {});
+        defer sorted_order.deinit();
+
         var seen_field: bool = false;
         for (connections.children orelse &.{}) |container_child| {
             // Declarations cannot appear between fields so once we see a field
@@ -57,22 +67,37 @@ fn run(
                 else => if (seen_field) break else continue,
             }
 
+            // Include any preceding doc comments in the captured range
             const first_token = token: {
                 var token = tree.firstToken(container_child);
                 while (tree.tokens.items(.tag)[token - 1] == .doc_comment) token -= 1;
                 break :token token;
             };
-
             const start = tree.tokenLocation(0, first_token);
 
             const last_token = tree.lastToken(container_child);
             const last = tree.tokenLocation(0, last_token);
             const last_token_value = tree.tokenSlice(last_token);
 
+            try insertion_order.append(container_child);
+            try sorted_order.add(.{
+                .start = .startOfToken(tree, first_token),
+                .end = .endOfToken(tree, last_token),
+                .name = "ok",
+                .node = container_child,
+            });
+
             std.debug.print("{s}: \n'''\n{s}\n'''\n\n", .{
                 @tagName(zlinter.shims.nodeTag(tree, container_child)),
                 tree.source[start.line_start + start.column .. last.line_start + last.column + last_token_value.len + 1],
             });
+        }
+
+        var i: usize = 0;
+        while (sorted_order.removeOrNull()) |field| : (i += 1) {
+            if (field.node != insertion_order.items[i]) {
+                std.debug.print("!!!Wrong oerder\n", .{});
+            }
         }
 
         std.debug.print("\n----------\n\n", .{});
@@ -87,6 +112,17 @@ fn run(
     else
         null;
 }
+
+const Field = struct {
+    start: zlinter.results.LintProblemLocation,
+    end: zlinter.results.LintProblemLocation,
+    name: []const u8,
+    node: std.zig.Ast.Node.Index,
+
+    fn cmp(_: void, lhs: Field, rhs: Field) std.math.Order {
+        return std.ascii.orderIgnoreCase(lhs.name, rhs.name);
+    }
+};
 
 test {
     std.testing.refAllDecls(@This());
