86c20ce93abb6e40d08f095cb48c2575c49c5ec8
Kurt Wagner <1269283+KurtWagner@users.noreply.github.com>
2025-07-18 14:35:36 +1000

    Syntax highlighting

--- explorer/explorer.js
+++ explorer/explorer.js
@@ -28,21 +28,19 @@ function parse(source) {
 }
 
 (() => {
-    var sync_tree_timeout = 0;
-    const sync_tree_debounce_ms = 200;
-
     const input_element = document.getElementById("input");
     const line_numbers_element = document.getElementById("line_numbers");
+    const highlight_element = document.getElementById("highlight");
     const tree_element = document.getElementById("tree");
 
     input_element.addEventListener('input', syncLineNumbers);
     input_element.addEventListener('input', syncTree);
 
-    input_element.innerText = default_code;
+    input_element.textContent = default_code;
     input_element.dispatchEvent(new Event('input'));
 
     function syncLineNumbers() {
-        const num_of_lines = input_element.innerText.split("\n").length;
+        const num_of_lines = input_element.textContent.split("\n").length;
 
         var fill = [];
         for (let i = 1; i <= num_of_lines; i++) fill.push(i);
@@ -53,91 +51,123 @@ function parse(source) {
 
 
     function syncTree() {
-        // Debounce to avoid parsing for every keystroke
-        clearTimeout(sync_tree_timeout);
-        sync_tree_timeout = setTimeout(() => {
-            parse(input_element.innerText)
-                .then(json => {
-                    console.log(json);
 
-                    const tokens = json.tokens;
-
-                    tree_element.innerHTML = "";
-                    tree_element.append(createTreeNode({
-                        tag: "root",
-                        body: json.body,
-                    }));
+        parse(input_element.textContent)
+            .then(json => {
 
 
-                    function createTreeNode(json_object) {
-                        const div = document.createElement('div');
-                        div.classList.add('tree__node');
 
-                        for (const [key, val] of Object.entries(json_object)) {
-                            if (key == "body") continue;
 
-                            const field_div = document.createElement('div');
-                            field_div.classList.add('tree__node__field');
+                const raw = input_element.textContent;
 
-                            if (key == "tag") {
-                                const tag_span = document.createElement('span');
-                                tag_span.classList.add('tree__node__field__tag');
-                                tag_span.textContent = val;
-                                field_div.append(tag_span);
-                            } else {
 
-                                const name_span = document.createElement('span');
-                                name_span.classList.add('tree__node__field__name');
-                                name_span.textContent = key;
-                                field_div.append(name_span);
+                console.log('raw', raw);
 
-                                const value_span = document.createElement('span');
-                                value_span.classList.add('tree__node__field__value');
-                                value_span.textContent = val;
-                                field_div.append(value_span);
+                var prev = 0;
+                const syntax = [];
+                for (const token of json.tokens) {
+                    syntax.push(raw.slice(prev, token.start));
 
-                                if (key.endsWith("_token")) {
-                                    const meta_span = document.createElement('span');
-                                    meta_span.classList.add('tree__node__field__meta');
-                                    meta_span.textContent = getTokenDescription(val);
-                                    field_div.append(meta_span);
-                                }
-                            }
+                    const slice = raw.slice(token.start, token.start + token.len);
+                    if (token.tag.startsWith('keyword_')) {
+                        syntax.push(`<span class="syntax-keyword">${slice}</span>`);
+                    } else if (token.tag.startsWith('string_literal')) {
+                        syntax.push(`<span class="syntax-string-literal">${slice}</span>`);
+                    } else if (['l_brace', 'r_brace'].includes(token.tag)) {
+                        syntax.push(`<span class="syntax-brace">${slice}</span>`);
+                    } else {
+                        syntax.push(slice);
+                    }
 
-                            div.append(field_div);
-                        }
+                    prev = token.start + token.len;
+                }
+                syntax.push(raw.slice(prev));
 
-                        if (json_object.body && json_object.body.length > 0) {
-                            const field_div = document.createElement('div');
-                            field_div.classList.add('tree__node__field');
+                console.log('syntax', syntax.join(""));
+                highlight_element.innerHTML = syntax.join("");
+
+
+
+
+                console.log(json);
+
+                const tokens = json.tokens;
+
+                tree_element.innerHTML = "";
+                tree_element.append(createTreeNode({
+                    tag: "root",
+                    body: json.body,
+                }));
+
+
+                function createTreeNode(json_object) {
+                    const div = document.createElement('div');
+                    div.classList.add('tree__node');
+
+                    for (const [key, val] of Object.entries(json_object)) {
+                        if (key == "body") continue;
+
+                        const field_div = document.createElement('div');
+                        field_div.classList.add('tree__node__field');
+
+                        if (key == "tag") {
+                            const tag_span = document.createElement('span');
+                            tag_span.classList.add('tree__node__field__tag');
+                            tag_span.textContent = val;
+                            field_div.append(tag_span);
+                        } else {
 
                             const name_span = document.createElement('span');
                             name_span.classList.add('tree__node__field__name');
-                            name_span.textContent = "body";
+                            name_span.textContent = key;
                             field_div.append(name_span);
 
-                            div.append(field_div);
+                            const value_span = document.createElement('span');
+                            value_span.classList.add('tree__node__field__value');
+                            value_span.textContent = val;
+                            field_div.append(value_span);
 
-                            for (const child_node of json_object.body) {
-                                div.append(createTreeNode(child_node))
+                            if (key.endsWith("_token")) {
+                                const meta_span = document.createElement('span');
+                                meta_span.classList.add('tree__node__field__meta');
+                                meta_span.textContent = getTokenDescription(val);
+                                field_div.append(meta_span);
                             }
                         }
 
-
-                        return div;
-
-
+                        div.append(field_div);
                     }
 
-                    function getTokenDescription(token) {
-                        const parts = [];
-                        for (const [key, val] of Object.entries(json.tokens[token] || {})) {
-                            parts.push(`${key}: ${val}`);
+                    if (json_object.body && json_object.body.length > 0) {
+                        const field_div = document.createElement('div');
+                        field_div.classList.add('tree__node__field');
+
+                        const name_span = document.createElement('span');
+                        name_span.classList.add('tree__node__field__name');
+                        name_span.textContent = "body";
+                        field_div.append(name_span);
+
+                        div.append(field_div);
+
+                        for (const child_node of json_object.body) {
+                            div.append(createTreeNode(child_node))
                         }
-                        return parts.join(", ");
                     }
-                });
-        }, sync_tree_debounce_ms);
+
+
+                    return div;
+
+
+                }
+
+                function getTokenDescription(token) {
+                    const parts = [];
+                    for (const [key, val] of Object.entries(json.tokens[token] || {})) {
+                        parts.push(`${key}: ${val}`);
+                    }
+                    return parts.join(", ");
+                }
+            });
     }
 })();
 
