8fb38d9764b54b680989eef4069d7bb6f1bbfe3f
Kurt Wagner <1269283+KurtWagner@users.noreply.github.com>
2025-07-29 12:58:09 +1000

    Add exclude extern and export options to decl naming, fn naming and max pos rules #84

--- integration_tests/test_cases/max_positional_args/max_positional_args.input.zig
+++ integration_tests/test_cases/max_positional_args/max_positional_args.input.zig
@@ -8,4 +8,7 @@ pub fn notGood(a: u32, b: u32, c: u32, d: u32, e: u32, f: u32) void {
     std.debug.print("{d} {d} {d} {d} {d} {d}", .{ a, b, c, d, e, f });
 }
 
+// extern are exluded by default
+extern fn tooManyButExtern(u32, u32, u32, u32, u32, u32) void;
+
 const std = @import("std");
