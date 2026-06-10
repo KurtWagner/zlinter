c0f100143b99b0e68d023e2da46a1d1669226eda
Kurt Wagner <1269283+KurtWagner@users.noreply.github.com>
2026-06-10 21:41:31 +1000

    Use multiarray for file store

--- src/lib/session/FileStore.zig
+++ src/lib/session/FileStore.zig
@@ -4,14 +4,18 @@ pub const FileId = enum(u32) {
     _,
 };
 
-/// Access using `ast(index)`.
-asts: std.ArrayList(std.zig.Ast),
+pub const File = struct {
+    /// AST of file.
+    tree: std.zig.Ast,
 
-/// Access using `source(index)`.
-sources: std.ArrayList([:0]const u8),
+    /// Source of file contents.
+    source: [:0]const u8,
 
-/// Access using `path(index)` (memory of string owned by `path_to_index`).
-paths: std.ArrayList([]const u8),
+    /// Absolute path to file.
+    abs_path: []const u8,
+};
+
+files: std.MultiArrayList(File),
 
 /// Normalized absolute path strings to file id. Don't access this
 /// directly, instead use `resolve(...)` and use the returned index with
@@ -19,27 +23,24 @@ paths: std.ArrayList([]const u8),
 path_to_index: std.StringHashMapUnmanaged(FileId),
 
 pub const empty: FileStore = .{
-    .asts = .empty,
-    .sources = .empty,
-    .paths = .empty,
+    .files = .empty,
     .path_to_index = .empty,
 };
 
 pub fn deinit(fs: *FileStore, gpa: std.mem.Allocator) void {
-    for (fs.asts.items) |*tree|
-        tree.deinit(gpa);
-
-    for (fs.sources.items) |source|
+    var slice = fs.files.slice();
+    for (
+        slice.items(.abs_path),
+        slice.items(.source),
+        slice.items(.tree),
+    ) |abs_path, source, *tree| {
+        gpa.free(abs_path);
         gpa.free(source);
+        tree.deinit(gpa);
+    }
 
-    var path_it = fs.path_to_index.keyIterator();
-    while (path_it.next()) |path|
-        gpa.free(path.*);
-
-    fs.asts.deinit(gpa);
-    fs.sources.deinit(gpa);
-    fs.path_to_index.deinit(gpa);
-    fs.paths.deinit(gpa); // Strings owned by `path_to_index`
+    fs.files.deinit(gpa);
+    fs.path_to_index.deinit(gpa); // Paths owned by File.
 }
 
 pub fn resolve(
@@ -63,8 +64,6 @@ pub fn resolve(
     ) catch unreachable;
     if (fs.path_to_index.get(normal_path)) |index| return index;
 
-    std.debug.assert(fs.asts.items.len == fs.sources.items.len);
-
     const source = try std.Io.Dir.cwd().readFileAllocOptions(
         io,
         normal_path,
@@ -78,43 +77,42 @@ pub fn resolve(
     var tree = try std.zig.Ast.parse(gpa, source, .zig);
     errdefer tree.deinit(gpa);
 
-    const path_key = try gpa.dupe(u8, normal_path);
-    errdefer gpa.free(path_key);
+    const abs_path = try gpa.dupe(u8, normal_path);
+    errdefer gpa.free(abs_path);
 
-    const id: FileId = @enumFromInt(@as(u32, @intCast(fs.asts.items.len)));
-    try fs.asts.append(gpa, tree);
-    errdefer _ = fs.asts.swapRemove(@intFromEnum(id));
+    const id: FileId = @enumFromInt(@as(u32, @intCast(fs.files.len)));
 
-    try fs.sources.append(gpa, source);
-    errdefer _ = fs.sources.swapRemove(@intFromEnum(id));
+    try fs.files.append(gpa, .{
+        .tree = tree,
+        .abs_path = abs_path,
+        .source = source,
+    });
+    errdefer _ = fs.files.swapRemove(@intFromEnum(id));
 
-    try fs.paths.append(gpa, path_key);
-    errdefer _ = fs.paths.swapRemove(@intFromEnum(id));
+    try fs.path_to_index.putNoClobber(gpa, abs_path, id);
+    errdefer _ = fs.path_to_index.remove(abs_path);
 
-    try fs.path_to_index.putNoClobber(gpa, path_key, id);
-    errdefer _ = fs.path_to_index.remove(path_key);
-
-    std.debug.print("File store: adding '{s}\n", .{path_key});
+    std.debug.print("File store: adding '{s}\n", .{abs_path});
 
     return id;
 }
 
 pub fn fileAst(fs: *const FileStore, id: FileId) *const std.zig.Ast {
     const index = @intFromEnum(id);
-    std.debug.assert(index < fs.asts.items.len);
-    return &fs.asts.items[index];
+    std.debug.assert(index < fs.files.len);
+    return &fs.files.items(.tree)[index];
 }
 
 pub fn fileSource(fs: *const FileStore, id: FileId) []const u8 {
     const index = @intFromEnum(id);
-    std.debug.assert(index < fs.asts.items.len);
-    return fs.sources.items[index];
+    std.debug.assert(index < fs.files.len);
+    return fs.files.items(.source)[index];
 }
 
 pub fn filePath(fs: *const FileStore, id: FileId) []const u8 {
     const index = @intFromEnum(id);
-    std.debug.assert(index < fs.asts.items.len);
-    return fs.paths.items[index];
+    std.debug.assert(index < fs.files.len);
+    return fs.files.items(.abs_path)[index];
 }
 
 const std = @import("std");
